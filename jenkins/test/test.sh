f1c429080f075049a346549a62047c7870c9b003
root <root@localhost.localdomain>
2020-06-29 11:46:32 -0700

    cambio de ruta

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,5 +4,6 @@ echo "################"
 echo "*** Testing  ***"
 echo "################"
 
-docker run -v /root/.m2:/root/.m2 -v $PWD/service-config-server:/service-config-server -w /service-config-server maven:3-alpine "$@"
+RUTA=/samba/anonymous/jenkins/jenkins_home/workspace/pipeline-demo
+docker run -v /root/.m2:/root/.m2 -v $RUTA/service-config-server:/service-config-server -w /service-config-server maven:3-alpine "$@"
 
