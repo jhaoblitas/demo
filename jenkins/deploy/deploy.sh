ce2af40896ba6f6847f9a7b3c82d6f9672540c88
root <root@localhost.localdomain>
2020-06-30 19:01:18 -0700

    cabios

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,4 +7,4 @@ echo $BUILD_TAG >> /tmp/.auth
 # Transfiere el archivo
 scp /tmp/.auth 192.168.126.200:/tmp/.auth 
 scp ./jenkins/deploy/publish.sh 192.168.126.200:/tmp/publish
-ssh 192.168.126.200 /tmp/publish
+ssh root@192.168.126.200 /tmp/publish
