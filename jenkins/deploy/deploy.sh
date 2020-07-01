7b7edc8b302be7f9ad71f071afcebe58064c9e7e
root <root@localhost.localdomain>
2020-06-30 19:08:36 -0700

    cambios

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 
 # Transfiere el archivo
-scp /tmp/.auth 192.168.126.200:/tmp/.auth 
-scp ./jenkins/deploy/publish.sh 192.168.126.200:/tmp/publish
+scp /tmp/.auth root@192.168.126.200:/tmp/.auth 
+scp ./jenkins/deploy/publish.sh root@192.168.126.200:/tmp/publish
 ssh root@192.168.126.200 /tmp/publish
